07348709fe87d8934d649a876028f580cd9c1365
roman <romankrasnov777@yandex.ru>
2019-03-19 02:35:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/exampleapp/MyDialog.java
@@ -0,0 +1,67 @@
+package com.example.exampleapp;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Build;
+import android.view.View;
+
+class MyDialog extends Dialog {
+
+    public MyDialog(Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    @Override
+    public void show() {
+        hideNavigationBar();
+        super.show();
+        hideNavigationBar();
+    }
+
+    @Override
+    public void dismiss() {
+        showNavigationBar();
+        super.dismiss();
+        showNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+            final View decorView = getWindow().getDecorView();
+            decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                        decorView.setSystemUiVisibility(flags);
+                    }
+                }
+            });
+        }
+    }
+
+
+    private void showNavigationBar() {
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+            final View decorView = getWindow().getDecorView();
+            decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                        decorView.setSystemUiVisibility(flags);
+                    }
+                }
+            });
+        }
+    }
+}
